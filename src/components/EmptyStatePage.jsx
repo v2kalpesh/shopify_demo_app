cddb52d87b1f6cff2ea58e32a0e11f36454e08b8
Kalpesh Jain <54436775+v2kalpesh@users.noreply.github.com>
2022-05-10 13:37:05 +0530

    first commit

---
+++ src/components/EmptyStatePage.jsx
@@ -0,0 +1,48 @@
+import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
+import { Page, Layout, EmptyState } from "@shopify/polaris";
+import { useState } from "react";
+
+
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
+
+export function EmptyStatePage({setSelection}) {
+
+    const [open, setOpen] = useState(false);
+
+    const handleSelection = (resources) => {
+        setOpen(false);
+        setSelection(resources.selection.map((product) => product.id));
+      };
+  return (
+    <Page>
+        <TitleBar
+            primaryAction={{
+                content:"Select Your Products",
+                onAction: () => setOpen(true),
+            }}
+        />
+
+        <ResourcePicker
+            resourceType="Product"
+            showVariants={false}
+            open={open}
+            onSelection={(res) => handleSelection(res)}
+            onCancel={() => setOpen(false)}
+        />
+
+      <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: "Select products",
+              onAction: () => setOpen(true),
+            }}
+            image={img}
+            imageContained
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+      </Layout>
+    </Page>
+  );
+}
